c4821fb1158e8a351d3f462c59cb20d744ed4ed2
Rodney940 <rodney.joo@gmail.com>
2017-08-26 22:12:38 +0900

    Base

---
+++ MeTheqoo/DownFileInfo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeTheqoo
+{
+	public class DownFileInfo
+	{
+		string OrigSrc { get; set; }
+		string FileType { get; set; }
+		string FileName { get; set; }
+		string FileNameOrg { get; set; }
+		Int64 FileSize { get; set; }
+		DateTime AddDateTime { get; set; }
+		DateTime SrcDateTime { get; set; }
+
+		public DownFileInfo(string src)
+		{
+			this.OrigSrc = src;
+		}
+
+		public DownFileInfo(string src, string FileName, string FileNameOrg)
+		{
+			this.OrigSrc = src;
+			this.FileName = FileName;
+			this.FileNameOrg = FileNameOrg;
+		}
+	}
+}
---
+++ MeTheqoo/DownloadFile.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.AccessControl;
+using System.Security.Principal;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MeTheqoo
+{
+	public class DownloadFile
+	{
+		protected String _SRC { get; set; }
+		protected String ImgFindKwd { get; set; }
+		private String _Content { get; set; }
+		protected String _ImgUrl { get; set; }
+		public Image _IMG { get; set; }
+		protected List<string> _imgNames = new List<string>();
+
+		public DownloadFile(String url) : base()
+		{
+			this._SRC = url;
+		}
+
+		protected void GetContentsFromSrc()
+		{
+			var webRequest = WebRequest.Create(_SRC);
+
+			using (var response = webRequest.GetResponse())
+			using (var content = response.GetResponseStream())
+			using (var reader = new StreamReader(content))
+			{
+				var strContent = reader.ReadToEnd();
+				this._Content = strContent.ToString();
+			}
+
+			if (!String.IsNullOrEmpty(this._Content))
+			{
+				MatchCollection tmp = System.Text.RegularExpressions.Regex.Matches(this._Content, this.ImgFindKwd);
+				foreach(Match img in tmp)
+				{
+					this._imgNames.Add(img.ToString());
+				}
+			}
+		}
+
+		protected bool DoDownloadFile()
+		{
+			// Download from web
+
+			foreach (string imgUrl in _imgNames)
+			{
+				string targetUrl = this.GetOriginalImageName(imgUrl);
+
+				WebClient webClient = new WebClient();
+
+				//have to add filename phrase
+				FileInfo tmpfi = MakeUnique(System.Environment.CurrentDirectory + @"\" + DateTime.Now.ToString("yyyyMMdd") + "_.jpg");
+
+				webClient.DownloadFile(targetUrl, tmpfi.FullName);
+
+				// read image from file, and delete tmp file?
+			}
+			return true;
+		}
+
+		public FileInfo MakeUnique(string path)
+		{
+			string dir = Path.GetDirectoryName(path);
+			string fileName = Path.GetFileNameWithoutExtension(path);
+			string fileExt = Path.GetExtension(path);
+
+			for (int i = 1; ; ++i)
+			{
+				path = Path.Combine(dir, fileName + i + fileExt);
+
+				if (!File.Exists(path))
+					return new FileInfo(path);
+			}
+		}
+
+		protected virtual string GetOriginalImageName(string imgUrl)
+		{
+			// orig をつけるかほかの方法
+
+			try { } catch (Exception) { }
+
+			return string.Empty;
+		}
+
+		protected virtual string GetOriginalMovieName(string imgUrl)
+		{
+			// orig をつけるかほかの方法
+
+			try { } catch (Exception) { }
+
+			return string.Empty;
+		}
+	}
+
+	public class DownloadTwitter : DownloadFile
+	{
+		public DownloadTwitter(String url) : base(url)
+		{
+			this.ImgFindKwd = @"data-image-url=.*";
+			
+			if (!String.IsNullOrEmpty(this._SRC))
+			{
+				this.GetContentsFromSrc();
+
+				DoDownloadFile();
+			}
+		}
+
+		protected override string GetOriginalImageName(string imgUrl)
+		{
+			// 後ろに :orig をつける
+			// 引数 imgUrl の文字列の例 "data-image-url="https://pbs.twimg.com/media/DDp82xwUMAEDOpz.jpg""
+
+			try
+			{
+				string[] spltRst = imgUrl.Split('\"');
+				if (spltRst.Length == 3)
+				{
+					string url = spltRst[1];
+					url = url + ":orig";
+
+					return url;
+				}
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message + ex.StackTrace);
+			}
+
+			return string.Empty;
+		}
+
+		protected override string GetOriginalMovieName(string imgUrl)
+		{
+			// orig をつけるかほかの方法
+
+			return string.Empty;
+		}
+	}
+}
